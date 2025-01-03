a7039bebffca9300697406732356ddc4cb21427d
litsynp <nocte55is@gmail.com>
2025-01-04 00:21:52 +0900

    feat: show date

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,18 @@
+import { DateTime } from "luxon";
+
 export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/style.css");
+
+  eleventyConfig.addFilter("postDate", date => {
+    const dateTimeFormat = DateTime.DATE_SHORT;
+    if (date && typeof date.getMonth === "function") {
+      return DateTime.fromJSDate(date).toLocaleString(dateTimeFormat);
+    }
+    if (typeof date === "object") {
+      return DateTime.fromObject(date).toLocaleString(dateTimeFormat);
+    }
+    return DateTime.fromISO(date).toLocaleString(dateTimeFormat);
+  });
 }
 
 export const config = {
