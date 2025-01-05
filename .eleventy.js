cab99e9806328c712ab27c67dc5b86f15396abf8
litsynp <nocte55is@gmail.com>
2025-01-05 22:31:56 +0900

    chore: set up CNAME

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,9 @@ import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/style.css");
 
+  // CNAME
+  eleventyConfig.addPassthroughCopy("./src/static/CNAME");
+
   // Image support
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
